cda1c278edc48166aeb6e045b3256fce81f068a4
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-12-04 23:42:52 +0500

    home page

--- work/app/page.tsx
+++ work/app/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '@/components/Navbar' 
+import Home from './pages/Home'
 
 const page = () => {
   return (
     
     <>
     <Navbar />
+    <Home/>
+
     <div>page</div>
     </>
   )
---
+++ work/app/pages/Home.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import React from 'react';
+import mainimage from '../assets/Frame 560.png';
+
+const Home = () => {
+  return (
+    <>
+      <div className="w-full bg-slate-300 flex flex-col lg:flex-row justify-center border-t-2 lg:space-x-10">
+        {/* Left Sidebar */}
+        <div className="lg:px-10 lg:pt-8 lg:border-r-2">
+          <div className="w-full flex flex-wrap sm:flex-col overflow-auto justify-between space-y-3 lg:w-[217px] lg:h-[344px] lg:p-5 bg-orange-500 border">
+            <p className='font-bold '>Women's Fashion</p>
+            <p>Electronics</p>
+            <p>Home & Lifestyle</p>
+            <p>Medicine</p>
+            <p>Sports & Outdoor</p>
+            <p>Babies & Toys</p>
+            <p>Groceries & Pets</p>
+            <p>Health & Beauty</p>
+          </div>
+        </div>
+
+        {/* Main Image Section */}
+        <div className="pt-8 lg:px-6 flex justify-center items-center">
+          <div className="w-full lg:w-[892px] h-[200px] sm:h-[344px] lg:h-[344px] bg-orange-500 flex justify-center items-center">
+            <Image
+              className="m-auto object-contain"
+              src={mainimage}
+              alt="Main Display"
+              width={892}
+              height={344}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
