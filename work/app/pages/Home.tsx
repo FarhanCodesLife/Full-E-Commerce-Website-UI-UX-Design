3aa11bd277fc3d5fa469a33af586a6f1944c2fcd
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-12-04 23:50:49 +0500

    all ok

--- work/app/pages/Home.tsx
+++ work/app/pages/Home.tsx
@@ -8,15 +8,15 @@ const Home = () => {
       <div className="w-full bg-slate-300 flex flex-col lg:flex-row justify-center border-t-2 lg:space-x-10">
         {/* Left Sidebar */}
         <div className="lg:px-10 lg:pt-8 lg:border-r-2">
-          <div className="w-full flex flex-wrap sm:flex-col overflow-auto justify-between space-y-3 lg:w-[217px] lg:h-[344px] lg:p-5 bg-orange-500 border">
-            <p className='font-bold '>Women's Fashion</p>
-            <p>Electronics</p>
-            <p>Home & Lifestyle</p>
-            <p>Medicine</p>
-            <p>Sports & Outdoor</p>
-            <p>Babies & Toys</p>
-            <p>Groceries & Pets</p>
-            <p>Health & Beauty</p>
+          <div className="w-full flex items-center space-x-7 p-2 overflow-auto justify-between lg:space-y-3 lg:w-[217px] lg:h-[344px] lg:p-5 bg-orange-500 border">
+            <p className='font-bold  '>WomenFashion</p>
+            <p className='font-bold '>Electronics</p>
+            <p className='font-bold '>Home&Lifestyle</p>
+            <p className='font-bold '>Medicine</p>
+            <p className='font-bold '>Sports&Outdoor</p>
+            <p className='font-bold '>Babies&Toys</p>
+            <p className='font-bold '>Groceries&Pets</p>
+            <p className='font-bold '>Health&Beauty</p>
           </div>
         </div>
 
