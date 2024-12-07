a1c832eb8adc9a1bdd32560ee71a63fe0df6e279
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-12-07 20:30:45 +0500

    all work

--- work/app/pages/Sales.tsx
+++ work/app/pages/Sales.tsx
@@ -20,16 +20,16 @@ const Sales = () => {
       }}
       className="w-full max-w-full"
     >
-        <div className='w-[100%]  flex flex-col md:flex-row md:space-x-10 space-y-4 md:space-y-0'>
+        <div className='w-[90%]  flex flex-col md:flex-row md:space-x-10 space-y-4 md:space-y-0'>
           <div className='space-y-3 w-full'>
             <div className='text-sm md:text-base'>Todays</div>
             
-            <div className='w-full bg-red-400 font-bold flex  sm:flex-row justify-between items-center gap-6'>
-              <div className='w-full  flex  sm:flex-row sm:space-x-8 space-y-2 sm:space-y-0 items-center'>
-                <h1 className='text-sm md:text-2xl bg-slate-300 lg:text-3xl font-bold'>Flash Sales</h1>
-                <h1 className='text-sm md:text-2xl bg-slate-500 lg:text-3xl font-bold'>00 : 23 : 19 : 53</h1>
+            <div className='w-full  font-bold flex  sm:flex-row justify-between items-center gap-6'>
+              <div className='w-full  flex  sm:flex-row sm:space-x-8 space-x-4 space-y-0 sm:space-y-0 items-center'>
+                <h1 className='text-sm md:text-2xl bg-slate-3 lg:text-3xl font-bold'>Flash Sales</h1>
+                <h1 className='text-sm md:text-2xl bg-slate00 lg:text-3xl font-bold'>00 : 23 : 19 : 53</h1>
               </div>
-              <div className="sm:w-1/4 justify-center bg-black space-x-5">
+              <div className="sm:w-1/4 w-1/2 justify-center  space-x-5">
 
                 <CarouselPrevious className="relative  " />
                 <CarouselNext className="relative" />
@@ -49,10 +49,10 @@ const Sales = () => {
       
       <CarouselContent>
       {[...Array(8)].map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/4 h-[280px]  lg:h-[350px]">
+          <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/4 px-5  lg:h-[350px]">
             <div className="p-1">
               <Card>
-                <CardContent className="flex  items-center h-[340px] justify-center p-3">
+                <CardContent className="flex  items-center h-[340px] justify-center p-5 sm:p-3">
               
             <div key={index} className=' bg-red-400 w-full h-full p-5'></div>
                       </CardContent>
